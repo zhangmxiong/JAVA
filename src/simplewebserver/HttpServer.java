86d99c201c120e029a117552879940968135df2a
zhangmxiong <zhangmx@succez.com>
2016-03-23 10:33:35 +0800

    commit

--- src/simplewebserver/HttpServer.java
+++ src/simplewebserver/HttpServer.java
@@ -1,6 +1,5 @@
 package simplewebserver;
 
-
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.net.InetAddress;
@@ -9,56 +8,55 @@ import java.io.OutputStream;
 import java.io.IOException;
 
 public class HttpServer {
-  //WEB_ROOT 是一个主目录
-  //public static final String WEB_ROOT = "d:";
-  // 设置关闭服务器命令
-  private static final String SHUTDOWN_COMMAND = "/EXIT";
-  private boolean shutdown = false;
+	// WEB_ROOT 是一个主目录
+	// public static final String WEB_ROOT = "d:";
+	// 设置关闭服务器命令
+	private static final String SHUTDOWN_COMMAND = "/EXIT";
+	private boolean shutdown = false;
 
-  public static void main(String[] args) {
-    HttpServer server = new HttpServer();
-    server.await();
-  }
+	public static void main(String[] args) {
+		HttpServer server = new HttpServer();
+		server.await();
+	}
 
-  public void await() {
-    ServerSocket serverSocket = null;
-    int port = 80;
-    try {
-    	//绑定本地电脑作为服务器，端口80，队列1.
-      serverSocket =  new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
-    }
-    catch (IOException e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-    // 开启服务器，直到关闭命令出现
-    while (!shutdown) {
-      Socket socket = null;
-      InputStream input = null;
-      OutputStream output = null;
-      try {
-        socket = serverSocket.accept(); //等待socket打开
-        input = socket.getInputStream();
-        output = socket.getOutputStream();
+	public void await() {
+		ServerSocket serverSocket = null;
+		int port = 80;
+		try {
+			// 绑定本地电脑作为服务器，端口80，队列1.
+			serverSocket = new ServerSocket(port, 1,
+					InetAddress.getByName("127.0.0.1"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		// 开启服务器，直到关闭命令出现
+		while (!shutdown) {
+			Socket socket = null;
+			InputStream input = null;
+			OutputStream output = null;
+			try {
+				socket = serverSocket.accept(); // 等待socket打开
+				input = socket.getInputStream();
+				output = socket.getOutputStream();
 
-        System.out.println("第一步：开始数据分析");
-        Request request = new Request(input);
-        request.parse();
+				System.out.println("第一步：开始数据分析");
+				Request request = new Request(input);
+				request.parse();
 
-        System.out.println("第二步：寻找资源链接");
-        Response response = new Response(output);
-        response.receiveRequest(request);
-        response.sendResource();
+				System.out.println("第二步：寻找资源链接");
+				Response response = new Response(output);
+				response.receiveRequest(request);
+				response.sendResource();
 
-        // 关闭通道
-        socket.close();
-        //判断关闭服务器命令是否存在
-        shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
-      }
-      catch (Exception e) {
-        e.printStackTrace();
-        continue;
-      }
-    }
-  }
+				// 关闭通道
+				socket.close();
+				// 判断关闭服务器命令是否存在
+				shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
+			} catch (Exception e) {
+				e.printStackTrace();
+				continue;
+			}
+		}
+	}
 }
--- src/simplewebserver/Request.java
+++ src/simplewebserver/Request.java
@@ -4,42 +4,42 @@ import java.io.InputStream;
 import java.io.IOException;
 import java.net.URLDecoder;
 
-import com.sun.xml.internal.fastinfoset.Decoder;
-
 public class Request {
 
-  private InputStream input;
-  private String uri;
+	private InputStream input;
+	private String uri;
 
-  public Request(InputStream input) {
-	  System.out.println("request请求开始，解析uri");
-    this.input = input;
-  }
-  public String getUri() {
-	    return uri;
-  }
-  public void parse() throws IOException {
+	public Request(InputStream input) {
+		System.out.println("request请求开始，解析uri");
+		this.input = input;
+	}
 
-    byte[] buffer = new byte[2048];
-    //读取输入流，并转换为字符串
-    input.read(buffer);
-    String rq = new String(buffer);
-    
-    System.out.println("输入流:"+input);    
-    System.out.println("网址数据:"+rq);
-    uri = parseUri(rq);
-    uri = URLDecoder.decode(uri,"utf-8");
-    System.out.println("截取后uri="+uri);
-  }
+	public String getUri() {
+		return uri;
+	}
 
-  public String parseUri(String requestString) {
-    int index1, index2;
-    index1 = requestString.indexOf(' ');
-    if (index1 != -1) {
-      index2 = requestString.indexOf(' ', index1 + 1);
-      if (index2 > index1)
-        return requestString.substring(index1 + 1, index2);
-    }
-    return null;
-  }
+	public void parse() throws IOException {
+
+		byte[] buffer = new byte[2048];
+		// 读取输入流，并转换为字符串
+		input.read(buffer);
+		String rq = new String(buffer);
+
+		System.out.println("input输入流:" + input);
+		System.out.println("网址数据:" + rq);
+		uri = parseUri(rq);
+		uri = URLDecoder.decode(uri, "utf-8");
+		System.out.println("截取后uri=" + uri);
+	}
+
+	public String parseUri(String requestString) {
+		int index1, index2;
+		index1 = requestString.indexOf(' ');
+		if (index1 != -1) {
+			index2 = requestString.indexOf(' ', index1 + 1);
+			if (index2 > index1)
+				return requestString.substring(index1 + 1, index2);
+		}
+		return null;
+	}
 }
